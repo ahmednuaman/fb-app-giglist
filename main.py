98359c93cfc1f812d8e4dfac9da07321f0c702f9
Ahmed Nuaman, Freelance Designer and Developer <ahmed@ahmednuaman.com>
2011-04-11 13:33:00 +0100

    set up main backend

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import base64
+import hmac
 import json
 import yaml
 
+from google.appengine.api import urlfetch
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
@@ -25,13 +28,69 @@ class MainHandler(webapp.RequestHandler):
     
     #def post(self):
     def get(self):
+        # get the config
+        c = Config().config
+        
+        # get the request data
+        r = ParseSignedRequest( self.request.get( 'signed_request' ) ).data
+        
+        # get fb access key
+        a = GetAccessKey( c[ 'app_id' ], c[ 'app_secret' ] ).token
+        
+        self.response.out.write(a)
+
+class Config(object):
+    def __init__(self):
         # read the config
         f = open( 'config.yaml', 'r' )
         
         # yamlize its ass
-        c = yaml.dump( yaml.load( f.read() ) )
+        c = yaml.load( f.read() )
         
+        # return it as a var
+        self.config = c
+
+class ParseSignedRequest(object):
+    def __init__(self, req):
+        # we split the request str into the encoded str and the payload
+        r = req.split( '.' )
         
+        self.request = r[ 1 ]
+        self.payload = r[ 0 ]
+        
+        # now we'll get the data
+        self.data = self._get_request_data()
+        
+    def _get_request_data(self):
+        # base64 decode the string
+        h = base64.decodestring( self.request + '==' )
+        
+        # now json decode its ass
+        return json.loads( h )
+
+class GetAccessKey(object):
+    def __init__(self, app_id, app_secret):
+        self.id = app_id
+        self.secret = app_secret
+        
+        # now let's make the request and get the token
+        self.token = self._get_access_token()
+        
+    def _get_access_token(self):
+        # format the url
+        u = 'https://graph.facebook.com/oauth/access_token?client_id=%(id)s&client_secret=%(secret)s&grant_type=client_credentials' \
+        % { 'id': self.id, 'secret': self.secret }
+        
+        # make the request
+        r = urlfetch.fetch( u )
+        
+        d = ''
+        
+        if r.status_code == 200:
+            # we just want the token thanks
+            d = r.content.replace( 'access_token=', '' )
+            
+        return d
 
 def main():
     application = webapp.WSGIApplication( [
